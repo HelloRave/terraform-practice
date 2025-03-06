d7bbb9a5ca3a67209d03a9ac00606aeae3573b5f
HelloRave <vxoweiwei@gmail.com>
2025-03-06 13:43:51 +0800

    Using data source to retrieve ami id

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -13,6 +13,26 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+data "aws_ami" "ami" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20250220.0-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 locals {
   app_server_tags = {
     CreationDate = "${formatdate("DD-MM-YYYY", timestamp())}"
@@ -21,7 +41,7 @@ locals {
 
 resource "aws_instance" "app_server" {
   count         = var.environment == "dev" ? 1 : 2
-  ami           = var.ami
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t2.micro"
 
   tags = {
