f729ff5569e176c6e213714002706c577389a02d
HelloRave <vxoweiwei@gmail.com>
2025-03-06 11:19:59 +0800

    Using locals

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -13,12 +13,19 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+locals {
+  app_server_tags = {
+    CreationDate = "${formatdate("DD-MM-YYYY", timestamp())}"
+  }
+}
+
 resource "aws_instance" "app_server" {
   count         = var.environment == "dev" ? 1 : 2
   ami           = var.ami
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Instance ${count.index}"
+    Name         = "Instance ${count.index}"
+    CreationDate = local.app_server_tags["CreationDate"]
   }
 }
