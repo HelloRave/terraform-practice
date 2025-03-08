c74f4b17449f41d093a14eeff83834406a027eea
HelloRave <vxoweiwei@gmail.com>
2025-03-08 14:40:29 +0800

    Provisioners

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -33,6 +33,13 @@ data "aws_ami" "ami" {
   }
 }
 
+data "aws_key_pair" "ec2_ssh_key" {
+  filter {
+    name   = "key-name"
+    values = ["terraform-key"]
+  }
+}
+
 locals {
   app_server_tags = {
     CreationDate = "${formatdate("DD-MM-YYYY", timestamp())}"
@@ -40,14 +47,58 @@ locals {
 }
 
 resource "aws_instance" "app_server" {
-  count         = var.environment == "dev" ? 2 : 1
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  count           = var.environment == "dev" ? 2 : 1
+  ami             = data.aws_ami.ami.image_id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.allow_tls.name]
+  key_name        = data.aws_key_pair.ec2_ssh_key.key_name
 
   tags = {
     Name         = "Instance ${count.index}"
     CreationDate = local.app_server_tags["CreationDate"]
   }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("./terraform-key.pem")
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo yum -y install httpd", "sudo systemctl start httpd"]
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-firewall"
+  description = "Managed from firewall"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_port" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.vpn_ip
+  from_port         = var.http_port
+  ip_protocol       = "tcp"
+  to_port           = var.http_port
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh_port" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.vpn_ip
+  from_port         = var.ssh_port
+  ip_protocol       = "tcp"
+  to_port           = var.ssh_port
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.vpn_ip
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 output "app_servers_arn" {
--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -6,3 +6,15 @@ variable "ami" {
   default     = "ami-0b03299ddb99998e9"
   description = "AMI for Singapore Region"
 }
+
+variable "vpn_ip" {
+  default = "0.0.0.0/0"
+}
+
+variable "ssh_port" {
+  default = 22
+}
+
+variable "http_port" {
+  default = 80
+}
